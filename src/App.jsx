85833a2dc77ed01e6ca50f79b752049700a77d88
Ruben Ramirez <rramirez110911@gmail.com>
2023-09-26 20:31:31 -0700

    imports home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import GlobalStyles from './components/GlobalStyles'
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [actorOne, setActorOne] = useState('')
+  const [actorTwo, setActorTwo] = useState('')
 
   return (
     <div>
+      <Home />
       <GlobalStyles />
     </div>
   )
