a84893e14a2c522433d21633b0aacc54ac2c1bd1
Ruben Ramirez <rramirez110911@gmail.com>
2023-09-27 22:00:33 -0700

    creates a state variable for the form data

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 function App() {
   const [actorOne, setActorOne] = useState('')
   const [actorTwo, setActorTwo] = useState('')
+  const [NamesObject, setNamesObject] = useState(null)
 
   return (
     <div>
