388e9ac7ee6f18fe6ee8f016d73a17d6d8fc143d
Ruben Ramirez <rramirez110911@gmail.com>
2023-10-02 21:48:30 -0700

    passes props down

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import Form from '../components/layout/Form'
 
 function Home() {
+  const [actorOne, setActorOne] = React.useState('')
+  const [actorTwo, setActorTwo] = React.useState('')
+  const [NamesObject, setNamesObject] = React.useState(null)
+
   return (
     <div>
-      <Form />
+      <Form
+        actorOne={actorOne}
+        actorTwo={actorTwo}
+        setActorOne={setActorOne}
+        setActorTwo={setActorTwo}
+      />
+      <p>1.{actorOne}</p>
+      <p>2.{actorTwo}</p>
     </div>
   )
 }
