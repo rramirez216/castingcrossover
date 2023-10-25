a79d38894e2eb611fed15e4bb958d5e339952190
Ruben Ramirez <rramirez110911@gmail.com>
2023-10-24 22:03:24 -0700

    removes useEffect and returns data and a function instead

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import axios from 'axios'
 
-function useFetch(url) {
+function useFetch() {
   const [data, setData] = React.useState(null)
 
-  React.useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let response = await axios.get(url)
+  // React.useEffect(() => {
 
-        setData(response.data.result)
-      } catch (error) {
-        console.log(error.message)
-      }
+  // }, [])
+  const fetchData = async (url) => {
+    try {
+      let response = await axios.get(url)
+      setData(response.data)
+    } catch (error) {
+      console.log(error.message)
     }
-    fetchData()
-  }, [])
+  }
 
-  return data
+  return { data, fetchData }
 }
 
 export default useFetch
