5cc657f239581bfe3eea819b11414382e3733019
Ruben Ramirez <rramirez110911@gmail.com>
2023-10-31 20:06:27 -0700

    refactors the fetchdata function to accent to strings as arguments and make the fetch calls using them

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,28 +2,44 @@ import React from 'react'
 import axios from 'axios'
 
 function useFetch() {
-  const [fetchedData, setFetchedData] = React.useState(null)
+  const [firstActorData, setFirstActorData] = React.useState(null)
+  const [secondActorData, setSecondActorData] = React.useState(null)
 
   // React.useEffect(() => {
 
   // }, [])
-  const fetchData = async (url) => {
+  const fetchData = async (first, second) => {
     try {
-      let responseForId = await axios.get(url)
-      let id = responseForId.data.results[0].id
+      let idForFirst = await axios.get(
+        `https://api.themoviedb.org/3/search/person?api_key=&query=${first}&include_adult=false&language=en-US&page=1`
+      )
+      let idForSecond = await axios.get(
+        `https://api.themoviedb.org/3/search/person?api_key=&query=${second}&include_adult=false&language=en-US&page=1`
+      )
 
-      let responseForCredits = await axios.get(
+      let id = idForFirst.data.results[0].id
+      let secondId = idForSecond.data.results[0].id
+
+      let creditsForFirst = await axios.get(
         `https://api.themoviedb.org/3/person/${id}/movie_credits?api_key=&language=en-US`
       )
-      let result = responseForCredits.data.cast
-      setFetchedData(result)
-      console.log(result)
+      let creditsForSecond = await axios.get(
+        `https://api.themoviedb.org/3/person/${secondId}/movie_credits?api_key=&language=en-US`
+      )
+
+      let firstResult = creditsForFirst.data.cast
+      setFirstActorData(firstResult)
+      console.log(firstResult)
+
+      let secondResult = creditsForSecond.data.cast
+      setSecondActorData(secondResult)
+      console.log(secondResult)
     } catch (error) {
       console.log(error.message)
     }
   }
 
-  return { fetchedData, fetchData }
+  return { firstActorData, secondActorData, fetchData }
 }
 
 export default useFetch
