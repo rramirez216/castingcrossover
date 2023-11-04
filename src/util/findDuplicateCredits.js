67fab793fbecedb1dc51f2848e29b9dfde836c15
Ruben Ramirez <rramirez110911@gmail.com>
2023-11-03 23:40:07 -0700

    filters out the items that have the property value of Self

--- src/util/findDuplicateCredits.js
+++ src/util/findDuplicateCredits.js
@@ -1,5 +1,13 @@
 function findDuplicateCredits(array1, array2) {
-  let combinedArr = [...array1, ...array2].map((obj) => obj.title)
+  let combinedArr = [...array1, ...array2]
+    .filter(
+      (obj) =>
+        obj.character !== 'Self' && obj.character !== 'Self (archive footage)'
+    )
+    .map((obj, index) => {
+      console.log({ name: obj.title, indice: index })
+      return obj.title
+    })
   let filteredArr = combinedArr.filter(
     (string, index) => combinedArr.indexOf(string) !== index
   )
