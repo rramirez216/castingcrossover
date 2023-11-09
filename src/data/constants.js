f5bf7685f1fae226506f64046434ee9e1c25b8d3
Ruben Ramirez <rramirez110911@gmail.com>
2023-11-08 20:59:11 -0800

    creates a constants obj and adds an array of possible messages to display when no matches are found

---
+++ src/data/constants.js
@@ -0,0 +1,14 @@
+const constantObj = {
+  noMatchesMessages: [
+    "Looks like these actors haven't shared the spotlight yet!",
+    "Sorry, but this duo hasn't graced the same screen.",
+    "Nope, they haven't co-starred in any films... yet!",
+    'No silver screen rendezvous for these two!',
+    "Their paths haven't crossed in movie magic.",
+    'Zero movie collisions for this actor pair.',
+    "They're still waiting for their movie destiny!",
+    'The movie universe is yet to unite these stars.',
+    'Zilch! Their filmographies remain unmerged.',
+    "Nada! Their cinematic worlds haven't collided.",
+  ],
+}
