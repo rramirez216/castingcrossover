062a74ce06064b6cb9adb9d581dd398f8c3c265d
Ruben Ramirez <rramirez110911@gmail.com>
2023-11-18 23:48:48 -0800

    fixes the filter function return value error

--- src/util/findDuplicateCredits.js
+++ src/util/findDuplicateCredits.js
@@ -4,16 +4,17 @@ function findDuplicateCredits(array1, array2) {
       (obj) =>
         obj.character !== 'Self' && obj.character !== 'Self (archive footage)'
     )
-    .map((obj, index) => {
-      // console.log({ name: obj.title, indice: index })
+    .map((obj) => {
       return {
         title: obj.title,
         release_date: obj.release_date,
         poster_path: obj.poster_path,
       }
     })
+
   let filteredArr = combinedArr.filter(
-    (string, index) => combinedArr.indexOf(string) !== index
+    (obj, index) =>
+      combinedArr.findIndex((item) => item.title === obj.title) !== index
   )
   return filteredArr
 }
