54fc6ddffa35112f72e1468d0b7f3b0b24cdb7ec
Ruben Ramirez <rramirez110911@gmail.com>
2023-12-01 13:22:22 -0800

    extends box shadow styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      boxShadow: {
+        ss: '0px 1.7px 2.6px -10px rgba(0, 0, 0, 0.075), 0px 5.6px 8.7px -10px rgba(0, 0, 0, 0.115), 0px 25px 39px -10px rgba(0, 0, 0, 0.2)',
+      },
       fontFamily: {
         playpen: ['"Playpen Sans"', ...defaultTheme.fontFamily.sans],
       },
