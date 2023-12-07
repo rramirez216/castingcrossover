3d01f55f81da626d8a94e18f6b0dcc0d34f25a9e
Ruben Ramirez <rramirez110911@gmail.com>
2023-12-06 20:18:12 -0800

    imports scroller method from react-scroll

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { scroller } from 'react-scroll'
 
 function Button() {
+  const options = {
+    delay: 500,
+    duration: 750,
+    smooth: true,
+  }
   return (
     <>
-      <button className='border-black border-2 rounded-md py-2' type='submit'>
+      <button
+        className='border-black border-2 rounded-md py-2'
+        onClick={() => scroller.scrollTo('list', options)}
+        type='submit'
+      >
         Search
       </button>
     </>
