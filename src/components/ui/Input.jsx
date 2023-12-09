586d6247442ead55f57e83453a211124e920af26
Ruben Ramirez <rramirez110911@gmail.com>
2023-12-09 15:36:24 -0800

    adds padding to the left

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -5,7 +5,7 @@ function Input({ inputName, handleChange, names, labelName, exampleName }) {
     <div className=''>
       <label className='block mr-4'>{labelName}:</label>
       <input
-        className='w-full h-10 block border-b-2 border-black bg-[#F7F7F7]'
+        className='w-full h-10 block border-b-2 border-black bg-[#F7F7F7] pl-2'
         value={names.inputName}
         name={inputName}
         type='text'
