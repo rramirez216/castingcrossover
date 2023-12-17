638dd0db723d1a6fff05f2a8657aa40fa0ce200e
Ruben Ramirez <rramirez110911@gmail.com>
2023-12-16 22:56:21 -0800

    adds react scroll logic and creates use effect function that handles the react scroll event

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,47 @@
 import React from 'react'
+import { scroller } from 'react-scroll'
+
 import Form from '../components/layout/Form'
 import List from '../components/layout/List'
 import useFetch from '../hooks/useFetch'
 import randomMessage from '../util/randomMessage'
 
 function Home() {
+  const [submitButtonClicked, setSubmitButtonClicked] = React.useState(false)
   const [names, setNames] = React.useState({
     actorOne: '',
     actorTwo: '',
   })
 
   const { list, fetchData } = useFetch()
-
-  // TODO: hide form after form submission and include a toggle for  the form
-  // TODO: display the roles of each actor in the movie
-  // TODO: fix over flow so header doesn't stay, maybe?
-  // TODO: figure out how to hide api key
-
-  let totalCoStarCredits
-  if (list) {
-    totalCoStarCredits = list.length > 0 ? list.length : randomMessage()
+  const options = {
+    delay: 300,
+    duration: 550,
+    smooth: true,
   }
-
   const handleChange = (event) => {
     setNames({
       ...names,
       [event.target.name]: event.target.value,
     })
   }
-
   const handleSubmit = (event) => {
     event.preventDefault()
     let first = names.actorOne.split(' ').join('%20')
     let second = names.actorTwo.split(' ').join('%20')
 
     fetchData(first, second)
+  }
+  React.useEffect(() => {
+    if (submitButtonClicked) {
+      scroller.scrollTo('list', options)
+      console.log('scroll!')
+    }
+  }, [submitButtonClicked])
+
+  let totalCoStarCredits
+  if (list) {
+    totalCoStarCredits = list.length > 0 ? list.length : randomMessage()
   }
 
   return (
@@ -43,6 +50,8 @@ function Home() {
         names={names}
         handleSubmit={handleSubmit}
         handleChange={handleChange}
+        setSubmitButtonClicked={setSubmitButtonClicked}
+        submitButtonClicked={submitButtonClicked}
       />
 
       <List list={list} total={totalCoStarCredits} />
