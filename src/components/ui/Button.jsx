33ac570e506d7609a46a40ffe02da68bb3f241f3
Ruben Ramirez <rramirez110911@gmail.com>
2023-12-28 20:05:34 -0800

    adds ternary to disable button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
+import { scroller } from 'react-scroll'
 
-function Button({ setSubmitButtonClicked, submitButtonClicked }) {
+function Button({ disableButton }) {
+  const options = {
+    delay: 300,
+    duration: 550,
+    smooth: true,
+  }
   return (
     <>
-      <button
-        className='border-black border-2 rounded-md py-2 bg-slate-950 text-white hover:scale-105 transition'
-        onClick={() => {
-          setSubmitButtonClicked(!submitButtonClicked)
-        }}
-        type='submit'
-      >
-        Search
-      </button>
+      {disableButton ? (
+        <button
+          className='border-black border-2 rounded-md py-2 bg-slate-950 text-white hover:scale-105 transition'
+          onClick={() => {
+            scroller.scrollTo('list', options)
+          }}
+          type='submit'
+          disabled
+        >
+          Search
+        </button>
+      ) : (
+        <button
+          className='border-black border-2 rounded-md py-2 bg-slate-950 text-white hover:scale-105 transition'
+          onClick={() => {
+            scroller.scrollTo('list', options)
+          }}
+          type='submit'
+        >
+          Search
+        </button>
+      )}
     </>
   )
 }
