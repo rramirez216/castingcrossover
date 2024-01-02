7a0f33879203a9d61ec4e5542aee11746b70d71a
Ruben Ramirez <rramirez110911@gmail.com>
2024-01-01 21:40:20 -0800

    creates new layout

--- src/components/layout/Item.jsx
+++ src/components/layout/Item.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Item({ item }) {
-  const { title, poster_path, release_date } = item
+  const { title, poster_path, release_date, role } = item
   let moviePoster = 'no image available'
 
   if (poster_path)
@@ -13,10 +13,20 @@ function Item({ item }) {
       />
     )
   return (
-    <div className='max-w-xs text-center shadow-ss p-4'>
-      <div className='mb-2'>{moviePoster}</div>
-      <p className=''>{`(${release_date.slice(0, 4)})`}</p>
-      <p>{title}</p>
+    // <div className='max-w-xs text-center shadow-ss p-4'>
+    //   <div className='mb-2'>{moviePoster}</div>
+    //   <p className=''>{`(${release_date.slice(0, 4)})`}</p>
+    //   <p>{title}</p>
+    // </div>
+
+    <div className='flex border-b first:border-t border-black p-4 gap-8'>
+      <div className='w-24'>{moviePoster}</div>
+      <div className='flex flex-col'>
+        <p>{title}</p>
+        <p>{role}</p>
+        <p>{role}</p>
+        <p>{`${release_date.slice(0, 4)}`}</p>
+      </div>
     </div>
   )
 }
