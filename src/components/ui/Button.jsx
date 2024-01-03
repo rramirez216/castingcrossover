bfc769ffa62b950c9f1dd5f9f171b1aa9efbee1b
Ruben Ramirez <rramirez110911@gmail.com>
2024-01-02 22:07:32 -0800

    removes disable button prop and ternary operator

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,37 +1,22 @@
 import React from 'react'
 import { scroller } from 'react-scroll'
 
-function Button({ disableButton }) {
+function Button() {
   const options = {
     delay: 300,
     duration: 550,
     smooth: true,
   }
   return (
-    <>
-      {disableButton ? (
-        <button
-          className='border-black border-2 rounded-md py-2 bg-slate-950 text-white hover:scale-105 transition'
-          onClick={() => {
-            scroller.scrollTo('list', options)
-          }}
-          type='submit'
-          disabled
-        >
-          Search
-        </button>
-      ) : (
-        <button
-          className='border-black border-2 rounded-md py-2 bg-slate-950 text-white hover:scale-105 transition'
-          onClick={() => {
-            scroller.scrollTo('list', options)
-          }}
-          type='submit'
-        >
-          Search
-        </button>
-      )}
-    </>
+    <button
+      className='border-black border-2 rounded-md py-2 bg-slate-950 text-white hover:scale-105 transition'
+      onClick={() => {
+        scroller.scrollTo('list', options)
+      }}
+      type='submit'
+    >
+      Search
+    </button>
   )
 }
 
