5cbc483c9f4fc1531ef77e7e6658f3c1a836dce4
Ruben Ramirez <rramirez110911@gmail.com>
2024-03-13 20:47:38 -0700

    testing env api calls

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,8 +1,15 @@
+import axios from 'axios'
+
 export const handler = async () => {
+  const url = `https://api.themoviedb.org/3/search/person?api_key=${
+    process.env.VITE_MOVIE_API_KEY
+  }&query=${'willem dafoe'}&include_adult=false&language=en-US&page=1`
+
+  const response = await axios.get(url)
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello World!',
+      message: response.data,
     }),
   }
 }
