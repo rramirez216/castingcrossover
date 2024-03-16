1a4c5ad4c01a17b10a66faaf4cfd983e6b7acac0
Ruben Ramirez <rramirez110911@gmail.com>
2024-03-15 20:24:50 -0700

    adds try/catch logic to the function

--- netlify/functions/getActorId.js
+++ netlify/functions/getActorId.js
@@ -1,17 +1,21 @@
 import axios from 'axios'
 
 export const handler = async (event) => {
-  const { name } = event.queryStringParameters
-  const url = `https://api.themoviedb.org/3/search/person?api_key=${process.env.VITE_MOVIE_API_KEY}&query=${name}&include_adult=false&language=en-US&page=1`
+  try {
+    const { name } = event.queryStringParameters
+    const url = `https://api.themoviedb.org/3/search/person?api_key=${process.env.VITE_MOVIE_API_KEY}&query=${name}&include_adult=false&language=en-US&page=1`
 
-  const response = await axios.get(url)
+    const response = await axios.get(url)
 
-  let actorId = response.data.results[0].id
+    const actorId = response.data.results[0].id
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      actorId,
-    }),
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        actorId,
+      }),
+    }
+  } catch (err) {
+    console.error(err)
   }
 }
