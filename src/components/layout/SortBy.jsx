c1a267082258925ff0dc01cae40374b0dbd26126
Ruben Ramirez <rramirez110911@gmail.com>
2024-05-19 22:19:58 -0700

    removes extra icon imports

--- src/components/layout/SortBy.jsx
+++ src/components/layout/SortBy.jsx
@@ -1,28 +1,22 @@
 import React from 'react'
-import {
-  ArrowDown01,
-  ArrowUp01,
-  ArrowDownAZ,
-  ArrowUpAZ,
-  ArrowUp,
-  ArrowDown,
-} from 'lucide-react'
+import { ArrowUp, ArrowDown } from 'lucide-react'
+import Input from '../ui/Input'
 
 function SortBy() {
   return (
-    <div>
+    <div className='text-base'>
       <label>Sort By: </label>
       <select>
         <option value=''>Release Date</option>
         <option value=''>Alphabetical</option>
       </select>
 
-      <input type='radio' name='asc' id='' />
-      <label htmlFor='asc'>
+      <input type='radio' name='sort' id='' />
+      <label htmlFor='sort'>
         <ArrowUp className='inline-block' />
       </label>
-      <input type='radio' name='desc' id='' />
-      <label htmlFor='desc'>
+      <input type='radio' name='sort' id='' />
+      <label htmlFor='sort'>
         <ArrowDown className='inline-block' />
       </label>
     </div>
