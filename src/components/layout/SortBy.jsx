9b89af2da5d50bbe7767c1e58929dc87547c55b5
Ruben Ramirez <rramirez110911@gmail.com>
2024-05-30 20:17:34 -0700

    removes inputs and iunused imports

--- src/components/layout/SortBy.jsx
+++ src/components/layout/SortBy.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { ArrowUp, ArrowDown } from 'lucide-react'
-import Input from '../ui/Input'
 import SortOrderButton from '../ui/SortOrderButton'
 
 function SortBy({
@@ -22,29 +20,6 @@ function SortBy({
         <option value='alphabetical'>Alphabetical</option>
       </select>
       <SortOrderButton sortOrder={sortOrder} setSortOrder={setSortOrder} />
-      {/* <input
-        type='radio'
-        name='sort'
-        id='ascending'
-        checked={radioOrder === 'ascending'}
-        value='ascending'
-        onChange={(event) => setRadioOrder(event.target.value)}
-      />
-
-      <label htmlFor='ascending'>
-        <ArrowUp className='inline-block' />
-      </label>
-      <input
-        type='radio'
-        name='sort'
-        id='descending'
-        checked={radioOrder === 'descending'}
-        value='descending'
-        onChange={(event) => setRadioOrder(event.target.value)}
-      /> */}
-      <label htmlFor='descending'>
-        <ArrowDown className='inline-block' />
-      </label>
     </div>
   )
 }
